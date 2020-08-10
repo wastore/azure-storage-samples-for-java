decab124bb7534873d05b2e85214af47f6836563
t-carome <t-carome@microsoft.com>
2020-08-10 16:47:59 -0700

    changing to camel case

--- E2E-ORS-Sample-For-Java/src/main/java/objectReplicationMonitor.java
+++ E2E-ORS-Sample-For-Java/src/main/java/objectReplicationMonitor.java
@@ -30,50 +30,50 @@ public class objectReplicationMonitor {
         Properties prop = new Properties();
         prop.load(input);
 
-        String source_container_name = prop.getProperty("source_container_name");
-        String source_storage_account_connection_string = prop.getProperty("source_storage_account_connection_string");
-        String destination_container_name = prop.getProperty("destination_container_name");
-        String destination_storage_account_connection_string = prop.getProperty("destination_storage_account_connection_string");
-        String blobs_to_replicate = prop.getProperty("blobs_to_replicate");
+        String sourceContainerName = prop.getProperty("sourceContainerName");
+        String sourceStorageAccountConnectionString = prop.getProperty("sourceStorageAccountConnectionString");
+        String destinationContainerName = prop.getProperty("destinationContainerName");
+        String destinationStorageAccountConnectionString = prop.getProperty("destinationStorageAccountConnectionString");
+        String blobsToReplicate = prop.getProperty("blobsToReplicate");
 
-        String[] replicated_blob_list = blobs_to_replicate.split(", ");
+        String[] replicatedBlobList = blobsToReplicate.split(", ");
 
         // sample of replication status
         System.out.println("\nThis is an example of using ORS...");
 
         // call method to setup blob in source container
-        setupSource(source_storage_account_connection_string, source_container_name, replicated_blob_list);
+        setupSource(sourceStorageAccountConnectionString, sourceContainerName, replicatedBlobList);
 
         // call method to check the replication status of the blob in source container
         System.out.println("\nReplicating blobs to destination container...");
         System.out.println("Once the replication process begins, the replication status will output...");
-        checkReplication(source_storage_account_connection_string, source_container_name, replicated_blob_list);
+        checkReplication(sourceStorageAccountConnectionString, sourceContainerName, replicatedBlobList);
 
         // call method to check the contents of a replicated blob
-        checkContents(source_storage_account_connection_string, source_container_name, destination_storage_account_connection_string, destination_container_name, replicated_blob_list);
+        checkContents(sourceStorageAccountConnectionString, sourceContainerName, destinationStorageAccountConnectionString, destinationContainerName, replicatedBlobList);
 
         // call method to archive replicated blobs
-        archiveReplicatedData(destination_storage_account_connection_string, destination_container_name, replicated_blob_list);
+        archiveReplicatedData(destinationStorageAccountConnectionString, destinationContainerName, replicatedBlobList);
 
     }
 
 
-    private static void setupSource(String connection_string, String container_name, String[] blob_list) {
+    private static void setupSource(String connectionString, String containerName, String[] blobList) {
         // this method is to upload a blob to the source container
 
         System.out.println("\nUploading blobs to the source container...");
 
         // creating blobserviceclient in order to make containerclient
-        BlobServiceClient sourceBlobServiceClient = new BlobServiceClientBuilder().connectionString(connection_string).buildClient();
+        BlobServiceClient sourceBlobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
 
         // get the container client
-        BlobContainerClient sourceContainerClient = sourceBlobServiceClient.getBlobContainerClient(container_name);
+        BlobContainerClient sourceContainerClient = sourceBlobServiceClient.getBlobContainerClient(containerName);
         if (!sourceContainerClient.exists()) {
             sourceContainerClient.create();
         }
 
         // iterate through list of blobs
-        for (String b : blob_list) {
+        for (String b : blobList) {
 
             // get blob client
             BlobClient sourceBlobClient = sourceContainerClient.getBlobClient(b);
@@ -88,28 +88,28 @@ public class objectReplicationMonitor {
     }
 
 
-    private static void checkReplication(String connection_string, String container_name, String[] blob_list) {
+    private static void checkReplication(String connectionString, String containerName, String[] blobList) {
         // this method is to check the replication status and the contents of the blob
 
         // creating blobserviceclient to access containerclient
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connection_string).buildClient();
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
 
         // get the container client to access blob client
-        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(container_name);
+        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
 
         int completed = 0;
         double ratio = 0;
-        int size = blob_list.length;
-        ArrayList completed_events = new ArrayList();
+        int size = blobList.length;
+        ArrayList completedEvents = new ArrayList();
 
         // while loop for continuous updating on replication status
         while (completed < size) {
 
             // iterate through list of blobs
-            for (String b : blob_list) {
+            for (String b : blobList) {
 
                 // check if event has already been completed
-                if (!completed_events.contains(b)) {
+                if (!completedEvents.contains(b)) {
                     // get blob client to access blob
                     BlobClient blobClient = blobContainerClient.getBlobClient(b);
 
@@ -126,7 +126,7 @@ public class objectReplicationMonitor {
                             String status = rule.getStatus().toString();
                             if (status.equals("complete")) {
                                 completed++;
-                                completed_events.add(b);
+                                completedEvents.add(b);
 
                             }
 
@@ -159,23 +159,23 @@ public class objectReplicationMonitor {
     }
 
 
-    private static void checkContents(String source_connection_string, String source_container, String destination_connection_string, String destination_container, String[] blob_list) throws FileNotFoundException {
+    private static void checkContents(String sourceConnectionString, String sourceContainer, String destinationConnectionString, String destinationContainer, String[] blobList) throws FileNotFoundException {
         // this method is to check that the contents of a replicated blob transferred correctly
 
-        String[] connection_string = {source_connection_string, destination_connection_string};
-        String[] container_name = {source_container, destination_container};
+        String[] connectionString = {sourceConnectionString, destinationConnectionString};
+        String[] containerName = {sourceContainer, destinationContainer};
 
-        for (int s = 0; s < connection_string.length; s++) {
+        for (int s = 0; s < connectionString.length; s++) {
             // creating blobserviceclient to access containerclient
-            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connection_string[s]).buildClient();
+            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString[s]).buildClient();
 
             // get the container client to access blob client
-            BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(container_name[s]);
+            BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(containerName[s]);
 
             // print out contents of one blob to show successful replication
-            BlobClient blobClient = blobContainerClient.getBlobClient(blob_list[0]);
+            BlobClient blobClient = blobContainerClient.getBlobClient(blobList[0]);
 
-            System.out.println("\nContent of " + blob_list[0] + " in the " + container_name[s] + " container:");
+            System.out.println("\nContent of " + blobList[0] + " in the " + containerName[s] + " container:");
 
             // create local file to store contents
             File localFile = new File("src/main/java/replicated_blob_content.txt");
@@ -198,17 +198,17 @@ public class objectReplicationMonitor {
     }
 
 
-    private static void archiveReplicatedData(String destination_connection_string, String destination_container, String[] blob_list) throws IOException {
+    private static void archiveReplicatedData(String destinationConnectionString, String destinationContainer, String[] blobList) throws IOException {
         // this method is to archive the replicated blobs
 
         System.out.println("\nTransferring replicated blobs in destination container to archive tier...");
 
-        for(String b : blob_list) {
+        for(String b : blobList) {
 
             Process p = null;
 
             // transferring blobs to archive
-            p = new ProcessBuilder("cmd.exe", "/c", "az storage blob set-tier --connection-string " + destination_connection_string + " --container-name " + destination_container + " --name " + b + " --tier Archive").start();
+            p = new ProcessBuilder("cmd.exe", "/c", "az storage blob set-tier --connection-string " + destinationConnectionString + " --container-name " + destinationContainer + " --name " + b + " --tier Archive").start();
             // Reading outputs from command line
             BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
             while ((r.readLine()) != null) {}
